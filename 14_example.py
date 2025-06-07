0aa0abe985821e24fe64e8d0136182aa8c4c7449
root <gitlab_admin_f3a050@example.com>
2025-06-07 21:29:10 +0500

    add 18 14

---
+++ 14_example.py
@@ -0,0 +1,36 @@
+# 1 Example № 21900
+
+for x in range(1, 2301):
+    c = 0
+    a = 7 ** 350 + 7**150 - x
+    while a > 0:
+        if a % 7 == 0:
+            c += 1
+        a //= 7
+    if c == 200:
+        print(x)
+
+# 2 Example № 21413
+
+from string import *
+
+for x in printable[:21]:
+    a = int(f'82934{x}2', 21) + int(f'2924{x}{x}7', 21) + int(f'67564{x}8', 21)
+    if a % 20 == 0:
+        print(x, a//20)
+
+# 3 Example № 21709
+
+m = []
+for x in range(1, 3001):
+    a = 4**210+4**110-x
+    c = 0
+    while a > 0:
+        if a % 4 == 0:
+            c += 1
+        a //= 4
+    m.append(c)
+    if c == 105:
+        print(x)
+        break
+print(max(m))
